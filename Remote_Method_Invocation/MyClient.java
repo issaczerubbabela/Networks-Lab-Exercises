521fd6ea473faa019c43d1310c8b356d1f3c0ac7
issaczerubbabela <126003111@sastra.ac.in>
2024-08-23 12:00:51 +0530

    stub and skeleton

---
+++ Remote_Method_Invocation/MyClient.java
@@ -0,0 +1,11 @@
+import java.rmi.*;
+
+public class MyClient {
+    public static void main(String args[]) {
+        try {
+            Adder stub = (Adder) Naming.lookup("rmi//localhost:5000/admin");
+            System.out.println(stub.add(6, 9));
+        } catch (Exception e) {
+        }
+    }
+}
