90cb6b44e3c7361cee5a5ca3e00bf1f9e908b10c
issaczerubbabela <126003111@sastra.ac.in>
2024-08-30 12:22:43 +0530

    rmi screenshot

--- Remote_Method_Invocation/MyClient.java
+++ Remote_Method_Invocation/MyClient.java
@@ -1,11 +1,21 @@
 import java.rmi.*;
+import java.util.Scanner;
 
 public class MyClient {
     public static void main(String args[]) {
         try {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter first number: ");
+            int num1 = scanner.nextInt();
+
+            System.out.print("Enter second number: ");
+            int num2 = scanner.nextInt();
+
             Adder stub = (Adder) Naming.lookup("rmi//localhost:5000/admin");
-            System.out.println(stub.add(6, 9));
+            System.out.println("Result: " + stub.add(num1, num2));
         } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
